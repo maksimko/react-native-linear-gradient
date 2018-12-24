91dd55563ee3378b4a8d6828a3fd0f6306e6f4a3
Maksim Bolshakov <maksimko@gmail.com>
2018-12-24 15:54:31 +0100

    Update to the last RN

--- Examples/AnimatedGradient/src/index.js
+++ Examples/AnimatedGradient/src/index.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import TimerMixin from 'react-timer-mixin';
-import LinearGradient from 'react-native-linear-gradient';
+import React from 'react'
+import createReactClass from 'create-react-class'
+import { StyleSheet, Text, View } from 'react-native'
+import TimerMixin from 'react-timer-mixin'
+import LinearGradient from 'react-native-linear-gradient'
 
 function incrementColor(color, step) {
-  const intColor = parseInt(color.substr(1), 16);
-  const newIntColor = (intColor + step).toString(16);
-  return `#${'0'.repeat(6 - newIntColor.length)}${newIntColor}`;
-};
+  const intColor = parseInt(color.substr(1), 16)
+  const newIntColor = (intColor + step).toString(16)
+  return `#${'0'.repeat(6 - newIntColor.length)}${newIntColor}`
+}
 
-const AnimatedGradient = React.createClass({
+const AnimatedGradient = createReactClass({
   mixins: [TimerMixin],
 
   getInitialState() {
     return {
       count: 0,
       colorTop: '#000000',
-      colorBottom: '#cccccc',
+      colorBottom: '#cccccc'
     }
   },
 
@@ -29,9 +26,9 @@ const AnimatedGradient = React.createClass({
       this.setState({
         count: this.state.count + 1,
         colorTop: incrementColor(this.state.colorTop, 1),
-        colorBottom: incrementColor(this.state.colorBottom, -1),
-      });
-    }, 20);
+        colorBottom: incrementColor(this.state.colorBottom, -1)
+      })
+    }, 20)
   },
 
   render() {
@@ -39,25 +36,30 @@ const AnimatedGradient = React.createClass({
       <View style={styles.container}>
         <LinearGradient
           colors={[this.state.colorTop, this.state.colorBottom]}
-          style={styles.gradient} />
-        <Text style={{color: this.state.colorTop}}>{this.state.colorTop}</Text>
-        <Text style={{color: this.state.colorBottom}}>{this.state.colorBottom}</Text>
+          style={styles.gradient}
+        />
+        <Text style={{ color: this.state.colorTop }}>
+          {this.state.colorTop}
+        </Text>
+        <Text style={{ color: this.state.colorBottom }}>
+          {this.state.colorBottom}
+        </Text>
       </View>
-    );
+    )
   }
-});
+})
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   gradient: {
     width: 200,
-    height: 200,
-  },
-});
+    height: 200
+  }
+})
 
-export default AnimatedGradient;
+export default AnimatedGradient
